21cfa6fd07189c77bbc6384a39129727da2d4e51
Neelesh Sharma <neelesh110493@gmail.com>
2026-04-26 13:04:37 +0800

    feat: add quick run scripts for data and backtesting

---
+++ scripts/download_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+freqtrade download-data --config user_data/config.json --days 30 -t 5m 15m 1h
---
+++ scripts/run_backtest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+freqtrade backtesting --config user_data/config.json --strategy CatBoostStrategy --freqaimodel CatboostRegressor --timerange 20260301-20260401
